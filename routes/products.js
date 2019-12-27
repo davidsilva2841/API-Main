ee6635d18091b0daeb37bf40b01777724bb987e7
ds <davidsilva2841+accounts+gitthub@gmail.com>
2019-12-27 11:54:11 -0600

    Added cors

--- routes/products.js
+++ routes/products.js
@@ -7,35 +7,35 @@ const {searchTable} = require('../services/mysql');
 // Test routes
 
 router.get('/test-get', (req, res) => {
-    console.log('GET /api/search/test');
     res.send('GET /api/search/test Working');
 });
 
 router.get('/test-search', (req, res) => {
-    console.log('GET /api/search/test');
     searchTable('Products', 'Name', 'Cow', true)
         .then(result => {
-            console.log('result: ', result);
             res.send(result);
         })
-        .catch(error => {
+        .catch(() => {
             res.sendStatus(500);
         });
 });
 
+// --------------------------------------------------------------------------------------------------
 
 // Searching products
+
 router.post('/search', (req, res) => {
-    searchTable('Products', 'Name', req.body.Name, true)
-        .then(result => {
-            console.log('result: ', result);
-            res.send(result);
-        })
-        .catch(error => {
-            res.sendStatus(500);
-        });
+    if (!req.body.Name) {
+        res.status(400).send('Missing request body Name')
+    } else {
+        searchTable('Products', 'Name', req.body.Name, true)
+            .then(result => {
+                res.send(result);
+            })
+            .catch(() => {
+                res.sendStatus(500);
+            });
+    }
 });
 
-
 module.exports = router;
-
